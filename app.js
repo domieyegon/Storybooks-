d1b06e07e5e1372dbd03891a4bc9b0cd3b325ac9
domieyegon <34186953+domieyegon@users.noreply.github.com>
2019-01-08 23:06:29 +0300

    Add files via upload
    THis website is build to help people share their stories

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const path= require('path');
+const exphbs= require('express-handlebars');
+const bodyParser= require('body-parser');
+const methodOverride= require('method-override');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+// Load  Models
+require('./models/User');
+require('./models/Story');
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Load Routes
+const index= require('./routes/index');
+const auth = require('./routes/auth');
+const stories= require('./routes/stories');
+
+// Load Keys
+const keys = require('./config/keys');
+
+//handlebars helpers
+const {
+    truncate,
+    stripTags,
+    formatDate,
+    select,
+    editIcon
+}= require('./helpers/hbs');
+
+// Map global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+const app = express();
+
+//body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+//parser application/json
+app.use(bodyParser.json());
+
+//method override middleware
+app.use(methodOverride('_method'));
+
+//handlebars middleware
+app.engine('handlebars', exphbs({
+   helpers: {
+       truncate: truncate,
+       stripTags: stripTags,
+       formatDate: formatDate,
+       select: select,
+       editIcon: editIcon
+   },
+    defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+});
+
+//Set static path to public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use Routes
+app.use('/', index);
+app.use('/auth', auth);
+app.use('/stories', stories);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+});
---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports= {
+    mongoURI: 'mongodb://dominic:dominic@ds141108.mlab.com:41108/storybook-dev',
+    googleClientID: '987298850127-oj89nefmhb1qqj8feob796lq7mfhca0k.apps.googleusercontent.com',
+    googleClientSecret: 'OEMNGvMwkt12y9WaKkwVNhu0'
+};
